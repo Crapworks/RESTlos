360c376ba25ab8fd1fa1e16cff6433a504f52a09
Crapworks <crapworks@github.com>
2012-12-17 17:48:50 +0100

    finished work on ldapauth module

--- utils/authentication/ldapauth.py
+++ utils/authentication/ldapauth.py
@@ -2,23 +2,62 @@
 # -*- coding: UTF-8 -*-
 
 import ldap
+import logging
 
 class AuthLDAP(object):
     """
     AuthLDAP: Authenticate a user against a ldap server
     """
 
-    def __init__(self, ldapserver="127.0.0.1", domain="WORKSHOP", ssl=True, groups=[]):
+    def __init__(self, 
+            ldapserver="127.0.0.1", 
+            userattr="uid",
+            searchattr="",
+            groupattr="memberof",
+            basedn="dc=example,dc=com", 
+            searchdn="dc=example,dc=com", 
+            ssl=True, 
+            groups=[]):
         self.ldapserver = ldapserver
-        self.domain = domain
+        self.basedn = basedn
+        self.groupattr = groupattr
+        self.searchdn = searchdn
+        self.userattr = userattr
         self.ssl = ssl
         self.groups = groups
 
+        if not searchattr:
+            self.searchattr = self.userattr
+        else:
+            self.searchattr = searchattr
+
+    def _initialize(self):
+        ldap.set_option(ldap.OPT_REFERRALS, 0)
+        if self.ssl:
+            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+
+        self.ldap = ldap.initialize("%s://%s:%d" % (
+            'ldaps' if self.ssl else 'ldap',
+            self.ldapserver,
+            636 if self.ssl else 389
+        ))
+
     def authenticate(self, username, password):
-        self.server = ldap.open(self.ldapserver)
+        self._initialize()
         try:
-            self.server.simple_bind_s(username, password)
-        except Exception:
+            self.ldap.simple_bind_s("%s=%s,%s" % (self.userattr, username, self.basedn), password)
+        except Exception, err:
+            logging.warn(str(err))
             return False
         else:
-            return True
+            if not self.groups:
+                return True
+
+            try:
+                result = self.ldap.search_s(self.searchdn, ldap.SCOPE_ONELEVEL, '(%s=%s)' % (self.searchattr, username), [str(self.groupattr)])
+                if result[0][1][self.groupattr][0] in self.groups:
+                    return True
+            except:
+                return False
+
+            return False
